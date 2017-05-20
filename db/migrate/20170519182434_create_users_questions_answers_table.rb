43b2edb722bbae6e21e440f4790135c63366fec7
darthpasta <liowj.nick@gmail.com>
2017-05-20 13:52:08 +0800

    Created model for answer,question and user

--- db/migrate/20170519182434_create_users_questions_answers_table.rb
+++ db/migrate/20170519182434_create_users_questions_answers_table.rb
@@ -1,23 +1,23 @@
 class CreateUsersQuestionsAnswersTable < ActiveRecord::Migration[5.0]
 	def change
-		create_table users do |t|
-			t.string: username
-			t.string: password 
+		create_table :users do |t|
+			t.string :username
+			t.string :password 
 
 			t.timestamps
 		end
 
-		create_table questions do |t|
-			t.integer: user_id
-			t.text: description
+		create_table :questions do |t|
+			t.integer :user_id
+			t.text :description
 
 			t.timestamps
 		end
 
-		create_table answers do |t|
-			t.integer: question_id
-			t.integer: user_id 
-			t.text: description
+		create_table :answers do |t|
+			t.integer :question_id
+			t.integer :user_id 
+			t.text :description
 
 			t.timestamps
 		end
