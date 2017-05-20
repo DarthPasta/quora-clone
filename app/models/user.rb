a60af56437bc17927920903267ee1a32be2a91db
darthpasta <liowj.nick@gmail.com>
2017-05-20 17:05:40 +0800

    Added bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+
 	has_many :questions
 	has_many :answers, :through => :question
 
@@ -9,5 +12,8 @@ class User < ActiveRecord::Base
 	# validates	:email, format: { with: /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i}
 	validates	:email, uniqueness: true
 
-	
+	has_secure_password
+	validates	:password_digest, presence: true
+
+
 end
