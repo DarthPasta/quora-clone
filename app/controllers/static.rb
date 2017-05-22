a29fa4a994aa8eb2fd91cafeb4a947bc5a122f57
darthpasta <liowj.nick@gmail.com>
2017-05-22 14:00:48 +0800

    Added error messages and sign up page

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,20 @@
 get '/' do
+
   erb :"static/index"
 end
+
+post '/signup' do
+	x = User.new 
+	x.username = params[:username]
+	x.email = params[:email]
+	x.password = params[:password] 
+
+	if x.save 
+		@user = x 
+		redirect to '/'
+	 else 
+	 	@user_errors = x 
+	end
+
+	erb :"static/index"
+end 
